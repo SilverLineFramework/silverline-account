14de68a67c190bc105dc4a961c7f4af88b3092a8
mwfarb <mwfarb@andrew.cmu.edu>
2024-04-16 09:18:53 -0400

    remove unneded

--- tests/mqtt/gen-jwt-secret-key.sh
+++ tests/mqtt/gen-jwt-secret-key.sh
@@ -7,14 +7,14 @@ read -p "Create RSA key pair for JWT? (y/N) " -r
 if [[ $REPLY =~ ^[Yy]$ ]]; then
   mkdir -p $KEY_OUT_PATH
   [ -f $KEY_OUT_PATH/jwt.private.pem ] && cp $KEY_OUT_PATH/jwt.private.pem $KEY_OUT_PATH/jwt.private.pem.bak
-  rm $KEY_OUT_PATH/*
+  #rm $KEY_OUT_PATH/*
   openssl genrsa -out $KEY_OUT_PATH/jwt.private.pem 4096
   openssl rsa -in $KEY_OUT_PATH/jwt.private.pem -pubout -outform PEM -out $KEY_OUT_PATH/jwt.public.pem
   openssl rsa -in $KEY_OUT_PATH/jwt.private.pem -RSAPublicKey_out -outform DER -out $KEY_OUT_PATH/jwt.public.der # mqtt auth plugin requires RSAPublicKey format
 fi
 
 # reset ownership of public keys
-[ -f $KEY_OUT_PATH/jwt.public.pem ] && chown $OWNER $KEY_OUT_PATH/jwt.public.pem
-[ -f $KEY_OUT_PATH/jwt.public.der ] && chown $OWNER $KEY_OUT_PATH/jwt.public.der
+# [ -f $KEY_OUT_PATH/jwt.public.pem ] && chown $OWNER $KEY_OUT_PATH/jwt.public.pem
+# [ -f $KEY_OUT_PATH/jwt.public.der ] && chown $OWNER $KEY_OUT_PATH/jwt.public.der
 
 # TODO: automate copy public key to
