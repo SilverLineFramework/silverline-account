1a637d8b00298526c1d33aa7d5d19cad007cf0c3
mwfarb <mwfarb@andrew.cmu.edu>
2024-04-16 08:42:38 -0400

    chore: update private key ext

--- tests/mqtt/gen-tls-private-key-cert.sh
+++ tests/mqtt/gen-tls-private-key-cert.sh
@@ -2,7 +2,8 @@
 
 KEY_OUT_PATH=keys
 
-openssl req -x509 -out $KEY_OUT_PATH/tls.host.crt -keyout $KEY_OUT_PATH/tls.host.private.key \
+mkdir -p $KEY_OUT_PATH
+openssl req -x509 -out $KEY_OUT_PATH/tls.host.crt -keyout $KEY_OUT_PATH/tls.host.private.pem \
   -newkey rsa:2048 -nodes -sha256 \
   -subj '/CN=localhost' -extensions EXT -config <( \
    printf "[dn]\nCN=localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
